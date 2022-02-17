67ce4abd69e6f5e55a6524ab4c5cb8ae1f00f61c
Geryon04 <juanjavierzerofour@gmail.com>
2022-02-17 17:45:06 -0300

    Racquet added

--- src/minitennis1/Game.java
+++ src/minitennis1/Game.java
@@ -3,6 +3,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.Ellipse2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,9 +13,30 @@ import javax.swing.JPanel;
 public class Game extends JPanel{
 	
 	Ball ball = new Ball(this);
+	Racquet racquet = new Racquet(this);
+	
+	public Game() {
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				racquet.keyReleased(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				racquet.keyPressed(e);
+			}
+		});
+		setFocusable(true);
+	}
 	
 	private void move() {
 		ball.move();
+		racquet.move();
 	}
 	
 	@Override
@@ -22,7 +45,7 @@ public class Game extends JPanel{
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		ball.paint(g2d);
-
+		racquet.paint(g2d);
 	}
 
 	public static void main(String[] args) throws InterruptedException{
