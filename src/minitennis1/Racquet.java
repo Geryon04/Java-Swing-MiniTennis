cb81e2c0d42219266f7f221bed14bcefc08b9c97
Geryon04 <juanjavierzerofour@gmail.com>
2022-02-18 19:28:14 -0300

    Collision added

--- src/minitennis1/Racquet.java
+++ src/minitennis1/Racquet.java
@@ -1,9 +1,13 @@
 package minitennis1;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 
 public class Racquet {
+	private static final int Y = 330;
+	private static final int WIDTH = 60;
+	private static final int HEIGHT = 10;
 	int x = 0;
 	int xa = 0;
 	private Game game;
@@ -13,12 +17,12 @@ public class Racquet {
 	}
 
 	public void move() {
-		if (x + xa > 0 && x + xa < game.getWidth()-60)
+		if (x + xa > 0 && x + xa < game.getWidth() - WIDTH)
 			x = x + xa;
 	}
 
 	public void paint(Graphics2D g) {
-		g.fillRect(x, 330, 60, 10);
+		g.fillRect(x, Y, WIDTH, HEIGHT);
 	}
 
 	public void keyReleased(KeyEvent e) {
@@ -30,5 +34,13 @@ public class Racquet {
 			xa = -1;
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
 			xa = 1;
+	}
+	
+	public Rectangle getBounds() {
+		return new Rectangle(x, Y, WIDTH, HEIGHT);
+	}
+	
+	public int getTopY() {
+		return Y - HEIGHT;
 	}
 }
